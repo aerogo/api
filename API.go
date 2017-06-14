e85cdd59975ad4548588850442401091e910c4b4
Eduard Urbach <e.urbach@gmail.com>
2017-06-15 01:26:11 +0200

    Improved collection support

--- API.go
+++ API.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"reflect"
 	"strings"
 
 	"github.com/aerogo/aero"
@@ -23,9 +24,72 @@ func New(root string, db Database) *API {
 
 // Install ...
 func (api *API) Install(app *aero.Application) {
-	for table := range api.db.Types() {
+	collection := reflect.TypeOf((*Collection)(nil)).Elem()
+
+	for table, objType := range api.db.Types() {
+		// GET route
 		route, handler := api.Get(table)
 		app.Get(route, handler)
+
+		// Collections
+		if reflect.PtrTo(objType).Implements(collection) {
+			// Add
+			route, handler = api.Collection(table, "add", func(coll Collection, item interface{}) error {
+				return coll.Add(item)
+			})
+			app.Post(route, handler)
+
+			// Remove
+			route, handler = api.Collection(table, "remove", func(coll Collection, item interface{}) error {
+				coll.Remove(item)
+				return nil
+			})
+			app.Post(route, handler)
+		}
+	}
+}
+
+// Collection ...
+func (api *API) Collection(table string, modificationName string, modify CollectionModification) (string, aero.Handle) {
+	objType := api.db.Type(table)
+	objTypeName := objType.Name()
+
+	route := api.root + strings.ToLower(objTypeName) + "/:id/" + modificationName
+	handler := api.CollectionHandler(objTypeName, modify)
+
+	return route, handler
+}
+
+// CollectionModification ...
+type CollectionModification func(Collection, interface{}) error
+
+// CollectionHandler ...
+func (api *API) CollectionHandler(objTypeName string, modify CollectionModification) aero.Handle {
+	return func(ctx *aero.Context) string {
+		objID := ctx.Get("id")
+		obj, err := api.db.Get(objTypeName, objID)
+
+		if err != nil {
+			return ctx.Error(http.StatusNotFound, "Collection not found", err)
+		}
+
+		body := ctx.RequestBody()
+		coll := obj.(Collection)
+		item := coll.TransformBody(body)
+
+		err = modify(coll, item)
+
+		if err != nil {
+			return ctx.Error(http.StatusInternalServerError, "Error adding item to list", err)
+		}
+
+		err = coll.Save()
+
+		if err != nil {
+			return ctx.Error(http.StatusInternalServerError, "Error saving list in database", err)
+		}
+
+		return "ok"
 	}
 }
 
--- Collection.go
+++ Collection.go
@@ -3,6 +3,12 @@ package api
 // Collection interface for lists, sets, etc.
 type Collection interface {
 	Add(interface{}) error
-	Remove(interface{}) error
+	Remove(interface{}) bool
 	Contains(interface{}) bool
+
+	// TransformBody returns an item that is passed to methods like Add, Remove, etc.
+	TransformBody(body []byte) interface{}
+
+	// Save saves the object in the database.
+	Save() error
 }
