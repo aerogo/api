92716cd89e1ca159abba34616c9b414755d942ba
Eduard Urbach <e.urbach@gmail.com>
2017-06-26 20:56:05 +0200

    Cleanup

--- API.go
+++ API.go
@@ -9,6 +9,8 @@ import (
 	"github.com/aerogo/aero"
 )
 
+var collectionInterface = reflect.TypeOf((*Collection)(nil)).Elem()
+
 // API ...
 type API struct {
 	root string
@@ -25,159 +27,162 @@ func New(root string, db Database) *API {
 
 // Install ...
 func (api *API) Install(app *aero.Application) {
-	collection := reflect.TypeOf((*Collection)(nil)).Elem()
-
 	for table, objType := range api.db.Types() {
-		// GET route
-		route, handler := api.Get(table)
+		api.RegisterTable(app, table, objType)
+	}
+}
+
+// RegisterTable registers a single table.
+func (api *API) RegisterTable(app *aero.Application, table string, objType reflect.Type) {
+	// Retrieve data
+	route, handler := api.Get(table)
+	app.Get(route, handler)
+
+	// Update data
+	route, handler = api.Update(table)
+
+	if route != "" && handler != nil {
+		app.Post(route, handler)
+	}
+
+	// Collections
+	if reflect.PtrTo(objType).Implements(collectionInterface) {
+		// Add
+		route, handler = api.Collection(table, "add", func(coll Collection, item interface{}) error {
+			return coll.Add(item)
+		})
+		app.Post(route, handler)
+
+		// Remove
+		route, handler = api.Collection(table, "remove", func(coll Collection, item interface{}) error {
+			coll.Remove(item)
+			return nil
+		})
+		app.Post(route, handler)
+
+		// Get
+		collectionType := api.db.Type(table)
+		collectionTypeName := collectionType.Name()
+
+		route = api.root + strings.ToLower(collectionTypeName) + "/:id/get/:item"
+		handler = func(ctx *aero.Context) string {
+			var collectionObj, item interface{}
+			var err error
+
+			collectionID := ctx.Get("id")
+			collectionObj, err = api.db.Get(collectionTypeName, collectionID)
+
+			if err != nil {
+				return ctx.Error(http.StatusNotFound, "Collection not found", err)
+			}
+
+			collection := collectionObj.(Collection)
+
+			itemID := ctx.Get("item")
+			item, err = collection.Get(itemID)
+
+			if err != nil {
+				return ctx.Error(http.StatusNotFound, "Item not found", err)
+			}
+
+			return ctx.JSON(item)
+		}
+
 		app.Get(route, handler)
 
-		// POST route
-		route, handler = api.Post(table)
+		// Get property
+		route = api.root + strings.ToLower(collectionTypeName) + "/:id/get/:item/:property"
+		handler = func(ctx *aero.Context) string {
+			var collectionObj, item interface{}
+			var err error
 
-		if route != "" && handler != nil {
-			app.Post(route, handler)
-		}
+			collectionID := ctx.Get("id")
+			collectionObj, err = api.db.Get(collectionTypeName, collectionID)
 
-		// Collections
-		if reflect.PtrTo(objType).Implements(collection) {
-			// Add
-			route, handler = api.Collection(table, "add", func(coll Collection, item interface{}) error {
-				return coll.Add(item)
-			})
-			app.Post(route, handler)
-
-			// Remove
-			route, handler = api.Collection(table, "remove", func(coll Collection, item interface{}) error {
-				coll.Remove(item)
-				return nil
-			})
-			app.Post(route, handler)
-
-			// Get
-			collectionType := api.db.Type(table)
-			collectionTypeName := collectionType.Name()
-
-			route = api.root + strings.ToLower(collectionTypeName) + "/:id/get/:item"
-			handler = func(ctx *aero.Context) string {
-				var collectionObj, item interface{}
-				var err error
-
-				collectionID := ctx.Get("id")
-				collectionObj, err = api.db.Get(collectionTypeName, collectionID)
-
-				if err != nil {
-					return ctx.Error(http.StatusNotFound, "Collection not found", err)
-				}
-
-				collection := collectionObj.(Collection)
-
-				itemID := ctx.Get("item")
-				item, err = collection.Get(itemID)
-
-				if err != nil {
-					return ctx.Error(http.StatusNotFound, "Item not found", err)
-				}
-
-				return ctx.JSON(item)
+			if err != nil {
+				return ctx.Error(http.StatusNotFound, "Collection not found", err)
 			}
 
-			app.Get(route, handler)
+			collection := collectionObj.(Collection)
 
-			// Get property
-			route = api.root + strings.ToLower(collectionTypeName) + "/:id/get/:item/:property"
-			handler = func(ctx *aero.Context) string {
-				var collectionObj, item interface{}
-				var err error
+			itemID := ctx.Get("item")
+			item, err = collection.Get(itemID)
 
-				collectionID := ctx.Get("id")
-				collectionObj, err = api.db.Get(collectionTypeName, collectionID)
+			if err != nil || item == nil {
+				return ctx.Error(http.StatusNotFound, "Item not found", err)
+			}
 
-				if err != nil {
-					return ctx.Error(http.StatusNotFound, "Collection not found", err)
-				}
+			propertyName := ctx.Get("property")
+			itemType := reflect.TypeOf(item).Elem()
+			itemValue := reflect.ValueOf(item).Elem()
+			property := itemValue.FieldByName(propertyName)
 
-				collection := collectionObj.(Collection)
+			if !property.IsValid() {
+				message := "Property '" + propertyName + "' does not exit on type " + itemType.Name() + "<br><br>Did you mean one of these?<ul>"
 
-				itemID := ctx.Get("item")
-				item, err = collection.Get(itemID)
+				for i := 0; i < itemType.NumField(); i++ {
+					field := itemType.Field(i)
 
-				if err != nil || item == nil {
-					return ctx.Error(http.StatusNotFound, "Item not found", err)
-				}
-
-				propertyName := ctx.Get("property")
-				itemType := reflect.TypeOf(item).Elem()
-				itemValue := reflect.ValueOf(item).Elem()
-				property := itemValue.FieldByName(propertyName)
-
-				if !property.IsValid() {
-					message := "Property '" + propertyName + "' does not exit on type " + itemType.Name() + "<br><br>Did you mean one of these?<ul>"
-
-					for i := 0; i < itemType.NumField(); i++ {
-						field := itemType.Field(i)
-
-						if field.Anonymous || !unicode.IsUpper([]rune(field.Name)[0]) {
-							continue
-						}
-
-						message += "<li>" + field.Name + "</li>"
+					if field.Anonymous || !unicode.IsUpper([]rune(field.Name)[0]) {
+						continue
 					}
-					message += "</ul>"
 
-					return ctx.Error(http.StatusBadRequest, message, nil)
+					message += "<li>" + field.Name + "</li>"
 				}
+				message += "</ul>"
 
-				return ctx.JSON(property.Interface())
+				return ctx.Error(http.StatusBadRequest, message, nil)
 			}
 
-			app.Get(route, handler)
-
-			// Update
-			route = api.root + strings.ToLower(collectionTypeName) + "/:id/update/:item"
-			handler = func(ctx *aero.Context) string {
-				var collectionObj, item interface{}
-				var err error
-
-				collectionID := ctx.Get("id")
-				collectionObj, err = api.db.Get(collectionTypeName, collectionID)
-
-				if err != nil {
-					return ctx.Error(http.StatusNotFound, "Collection not found", err)
-				}
-
-				collection := collectionObj.(Collection)
-
-				// Authorize
-				err = collection.Authorize(ctx)
-
-				if err != nil {
-					return ctx.Error(http.StatusForbidden, "Not authorized", err)
-				}
-
-				itemID := ctx.Get("item")
-				body := ctx.RequestBody()
-				item = collection.PostBody(body)
-
-				// Edit
-				err = collection.Update(itemID, item)
-
-				if err != nil {
-					return ctx.Error(http.StatusBadRequest, "Item could not be edited", err)
-				}
-
-				// Save
-				err = collection.Save()
-
-				if err != nil {
-					return ctx.Error(http.StatusInternalServerError, "Collection could not be saved", err)
-				}
-
-				return "ok"
-			}
-
-			app.Post(route, handler)
+			return ctx.JSON(property.Interface())
 		}
+
+		app.Get(route, handler)
+
+		// Update
+		route = api.root + strings.ToLower(collectionTypeName) + "/:id/update/:item"
+		handler = func(ctx *aero.Context) string {
+			var collectionObj, item interface{}
+			var err error
+
+			collectionID := ctx.Get("id")
+			collectionObj, err = api.db.Get(collectionTypeName, collectionID)
+
+			if err != nil {
+				return ctx.Error(http.StatusNotFound, "Collection not found", err)
+			}
+
+			collection := collectionObj.(Collection)
+
+			// Authorize
+			err = collection.Authorize(ctx)
+
+			if err != nil {
+				return ctx.Error(http.StatusForbidden, "Not authorized", err)
+			}
+
+			itemID := ctx.Get("item")
+			body := ctx.RequestBody()
+			item = collection.PostBody(body)
+
+			// Edit
+			err = collection.Update(itemID, item)
+
+			if err != nil {
+				return ctx.Error(http.StatusBadRequest, "Item could not be edited", err)
+			}
+
+			// Save
+			err = collection.Save()
+
+			if err != nil {
+				return ctx.Error(http.StatusInternalServerError, "Collection could not be saved", err)
+			}
+
+			return "ok"
+		}
+
+		app.Post(route, handler)
 	}
 }
 
@@ -262,8 +267,8 @@ func (api *API) Get(table string) (string, aero.Handle) {
 	return route, handler
 }
 
-// Post ...
-func (api *API) Post(table string) (string, aero.Handle) {
+// Update ...
+func (api *API) Update(table string) (string, aero.Handle) {
 	objType := api.db.Type(table)
 	objTypeName := objType.Name()
 	editableInterface := reflect.TypeOf((*Editable)(nil)).Elem()
