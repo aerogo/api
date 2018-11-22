a30ebf185d78b32280607f336579d42d1c253ed3
Eduard Urbach <e.urbach@gmail.com>
2018-11-22 13:22:17 +0900

    Added Action.Name

--- Action.go
+++ Action.go
@@ -5,6 +5,7 @@ import "github.com/aerogo/aero"
 // Action defines a single action on a given datatype.
 type Action struct {
 	Table string
+	Name  string
 	Route string
 	Run   func(obj interface{}, ctx *aero.Context) error
 }
